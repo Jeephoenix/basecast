730727f5a44a7f9866a5c3f2efbf5be71cf86153
Jeephoenix <168826535+Jeephoenix@users.noreply.github.com>
2026-03-31 13:38:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = { fs: false, net: false, tls: false, ws: false };
     config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.ignoreWarnings = [
+      { module: /node_modules\/@metamask\/sdk/ },
+      { module: /node_modules\/@react-native-async-storage/ },
+    ];
     return config;
   },
 };
